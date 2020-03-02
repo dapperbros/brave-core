e5afc73eea19013c25ae636d9c1d1c996b3dc2ee
NejcZdovc <nejc.zdovc@3zsistemi.si>
2020-03-02 11:40:23 +0100

    Zero balance check is not needed in uphold flow anymore

--- vendor/bat-native-ledger/src/bat/ledger/internal/uphold/uphold_wallet.cc
+++ vendor/bat-native-ledger/src/bat/ledger/internal/uphold/uphold_wallet.cc
@@ -96,8 +96,6 @@ void UpholdWallet::OnGenerate(
 
   wallet_ptr->user_name = user.name;
 
-  bool allow_zero_balance = false;
-
   if (user.status != UserStatus::OK) {
     wallet_ptr->status = ledger::WalletStatus::PENDING;
   }
@@ -107,23 +105,13 @@ void UpholdWallet::OnGenerate(
 
   if (current_status == ledger::WalletStatus::CONNECTED &&
       wallet_ptr->status == ledger::WalletStatus::VERIFIED) {
-    allow_zero_balance = true;
     ledger_->TransferTokens(wallet_ptr->Clone(), [](const ledger::Result){});
-  }
-
-  // if we don't have anon address we need to force claim so that server
-  // can save it
-  const std::string anon_address =
-      ledger_->GetStringState(ledger::kStateUpholdAnonAddress);
-  if (anon_address.empty()) {
-    allow_zero_balance = true;
   }
 
   if (wallet_ptr->address.empty()) {
     auto card_callback = std::bind(
       &UpholdWallet::OnCreateCard,
       this,
-      allow_zero_balance,
       *wallet_ptr,
       callback,
       _1,
@@ -134,10 +122,7 @@ void UpholdWallet::OnGenerate(
 
   if (user.verified) {
     ledger_->TransferTokens(wallet_ptr->Clone(), [](const ledger::Result){});
-    uphold_->TransferAnonToExternalWallet(
-        std::move(wallet_ptr),
-        allow_zero_balance,
-        callback);
+    uphold_->TransferAnonToExternalWallet(std::move(wallet_ptr), callback);
     return;
   }
 
@@ -146,7 +131,6 @@ void UpholdWallet::OnGenerate(
 }
 
 void UpholdWallet::OnCreateCard(
-    const bool allow_zero_balance,
     const ledger::ExternalWallet& wallet,
     ledger::ExternalWalletCallback callback,
     const ledger::Result result,
@@ -161,10 +145,7 @@ void UpholdWallet::OnCreateCard(
   wallet_ptr = GenerateLinks(std::move(wallet_ptr));
 
   if (wallet_ptr->status == ledger::WalletStatus::VERIFIED) {
-    uphold_->TransferAnonToExternalWallet(
-        std::move(wallet_ptr),
-        allow_zero_balance,
-        callback);
+    uphold_->TransferAnonToExternalWallet(std::move(wallet_ptr), callback);
     return;
   }
 
